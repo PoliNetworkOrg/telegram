29ebda73a286c5d431f34dc2e606dfc7b01832f2
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2025-03-25 03:23:07 +0100

    feat: redis and userid

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Telegraf } from 'telegraf'
 import { logger } from './logger.ts'
 import type { Message } from 'telegraf/typings/core/types/typegram'
+import { message } from "telegraf/filters"
+import { getTelegramId, setTelegramId } from './redis.ts'
+
+//import { client } from './redis.ts'
 
 function getText(message: Message): string | null {
   return 'text' in message && message.text !== undefined ? message.text : null
@@ -24,13 +28,24 @@ bot.telegram.setMyCommands([
   { command: 'ping', description: 'Test command' },
 ])
 
+bot.on(message("text"), (ctx, next) => {
+  next()
+  if (ctx.chat.type === "private") return;
+
+  const { username, id } = ctx.message.from
+  if (username) {
+    setTelegramId(username, id)
+  }
+})
+
+
 bot.help(async (ctx) => {
-  if (ctx.chat.type !== "private") {
+  if (ctx.chat.type !== 'private') {
     await ctx.deleteMessage(ctx.message.message_id)
-    return;
+    return
   }
 
-  ctx.reply('This is a test!') 
+  ctx.reply('This is a test!')
 })
 
 bot.command('ping', (ctx) => ctx.reply('pong'))
@@ -64,6 +79,31 @@ bot.command('del', async (ctx) => {
     }, 3000)
   }
 })
+
+bot.command('userid', async (ctx) => {
+  if (ctx.message.chat.type !== 'private') {
+    return
+  }
+
+  const msg = ctx.message.text
+  const username = msg.split(" ")[1]?.replace("@", "")
+  if (!username){
+    logger.error("[userid] the first param must be a username")
+    return;
+  }
+
+  const id = await getTelegramId(username)
+  if (!id) { 
+    logger.warn(`[userid] username @${username} not in our cache`)
+    await ctx.reply(`Username @${username} not in our cache`)
+    return;
+  }
+
+  
+  await ctx.reply(`Username \`@${username}\`\nid: \`${id}\``, { parse_mode: "MarkdownV2" })
+})
+
+
 bot.launch(() => logger.info('Bot started!'))
 
 process.once('SIGINT', () => bot.stop('SIGINT'))
---
+++ src/redis.ts
@@ -0,0 +1,24 @@
+import redis from 'redis'
+
+export const PREFIX = "tsbot"
+export const key = (key: string) => `${PREFIX}:${key}`
+
+export const client = await redis
+  .createClient({
+    socket: {
+      host: process.env.REDIS_HOST,
+      port: parseInt(process.env.REDIS_PORT!),
+    },
+  })
+  .connect()
+
+export async function getTelegramId(username: string): Promise<number | null> {
+  const res = await client.get(key(`username:${username}:id`))
+  if (!res) return null
+
+  return parseInt(res)
+}
+
+export async function setTelegramId(username: string, id: number): Promise<void> {
+  await client.set(key(`username:${username}:id`), id)
+}
