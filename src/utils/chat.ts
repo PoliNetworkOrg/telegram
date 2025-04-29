e1b31f1ac26e896d5df0d398a8003102810a949e
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2025-04-30 01:34:33 +0200

    chore: format

--- src/utils/chat.ts
+++ src/utils/chat.ts
@@ -1,12 +1,11 @@
 export function padChatId(chatId: number): number {
-  if (chatId < 0) return chatId;
+  if (chatId < 0) return chatId
 
-  const str = chatId.toString();
-  if (str.length === 13) return -chatId;
+  const str = chatId.toString()
+  if (str.length === 13) return -chatId
 
-  const padding = "1" + "0".repeat(12 - str.length);
+  const padding = "1" + "0".repeat(12 - str.length)
 
   // Prepend the padding to the input string
-  return parseInt(`-${padding}${chatId}`);
+  return parseInt(`-${padding}${chatId}`)
 }
-
