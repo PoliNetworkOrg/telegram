47b7fc16fc0d0f60b8618a5fbb9f8bb3b0e5c121
Tommaso Morganti <tommaso.morganti01@gmail.com>
2025-05-20 17:57:56 +0200

    feat: handle promise rejection (#23)
    * feat: handle promise rejection
    
    * fix dum dum typo
    
    * fix: single fatal
    
    ---------
    
    Co-authored-by: Lorenzo Corallo <lorenzo.corallo@outlook.it>

--- src/bot.ts
+++ src/bot.ts
@@ -4,7 +4,7 @@ import { autoRetry } from "@grammyjs/auto-retry"
 import { hydrate } from "@grammyjs/hydrate"
 import { hydrateReply, parseMode } from "@grammyjs/parse-mode"
 import { run, sequentialize } from "@grammyjs/runner"
-import { Bot } from "grammy"
+import { Bot, GrammyError, HttpError } from "grammy"
 
 import { apiTestQuery } from "./backend"
 import { commands } from "./commands"
@@ -15,6 +15,7 @@ import { checkUsername } from "./middlewares/check-username"
 import { messageLink } from "./middlewares/message-link"
 import { MessageStorage } from "./middlewares/message-storage"
 import { redis } from "./redis"
+import { fmt } from "./utils/format"
 import { setTelegramId } from "./utils/telegram-id"
 
 const TEST_CHAT_ID = -1002669533277
@@ -47,6 +48,40 @@ bot.on("message", messageStorage.middleware)
 bot.on("my_chat_member", botJoin({ logChatId: TEST_CHAT_ID }))
 bot.on("message", checkUsername)
 
+bot.catch(async (err) => {
+  const { error } = err
+  const msg = fmt(
+    ({ b, code, n, i, codeblock, u, link }) => {
+      const lines = [n`⚠️ An error occured inside the middleware stack`, b`${u`${err.message}`}\n`]
+      if (error instanceof GrammyError) {
+        lines.push(
+          n`${u`${b`grammY Error`} while calling method`}: ${link(
+            error.method,
+            `https://core.telegram.org/bots/api#${error.method.toLowerCase()}`
+          )} (${code`${error.error_code}`})`
+        )
+        lines.push(n`Description: ${i`${error.description}`}`)
+        lines.push(n`Payload:`, codeblock`${JSON.stringify(error.payload, null, 2)}`)
+      } else if (error instanceof HttpError) {
+        lines.push(n`${u`HTTP Error`}: ${code`${error.name}`}`)
+      } else if (error instanceof Error) {
+        lines.push(n`Unknown Error: ${code`${error.name}`}`)
+      } else {
+        lines.push(n`Something besides an ${code`Error`} has been thrown, check the logs for more info`)
+      }
+      return lines
+    },
+    { sep: "\n" }
+  )
+
+  const e = err as { ctx: { api?: unknown } }
+  delete e.ctx.api // LEAKS API TOKEN IN LOGS!!
+  logger.error(e)
+  await bot.api.sendMessage(TEST_CHAT_ID, msg).catch(() => {
+    logger.error("Couldn't send the middleware stack error through the bot")
+  })
+})
+
 const runner = run(bot)
 
 let terminateStarted = false // this ensure that it's called only once. otherwise strange behaviours
@@ -64,3 +99,25 @@ async function terminate(signal: NodeJS.Signals) {
 }
 process.on("SIGINT", () => void terminate("SIGINT"))
 process.on("SIGTERM", () => void terminate("SIGTERM"))
+
+process.on("unhandledRejection", (reason: Error, promise) => {
+  logger.fatal({ reason, promise }, "UNHANDLED PROMISE REJECTION")
+  void bot.api
+    .sendMessage(
+      TEST_CHAT_ID,
+      fmt(
+        ({ b, u, n, i, codeblock }) => [
+          b`${u`🛑 UNHANDLED PROMISE REJECTION`}`,
+          n`${reason.name}`,
+          i`${reason.message}`,
+          codeblock`${reason.stack ?? `no stack trace available`}`,
+        ],
+        {
+          sep: "\n",
+        }
+      )
+    )
+    .catch(() => {
+      logger.fatal("Couldn't send the 'unhandled rejection' error message through the bot, how ironic ")
+    })
+})
