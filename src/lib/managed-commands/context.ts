c30d25916fa0f74c99a047adc8c3ef58bef6ed2e
Lorenzo <lorenzo.corallo@outlook.it>
2025-07-28 18:49:03 +0200

    fix: adjust context flavor order, otherwise ctx is never in some cases
    this requires at the moment to add the bang in `ctx.message!` (see diff)
    but it is actually needed (it wasn't before this change, but probably
    the context was incorrectly typed out)
    
    if in future this change requires more bang to be added, we can rethink
    of the whole thing, but for the moment it works just fine

--- src/lib/managed-commands/context.ts
+++ src/lib/managed-commands/context.ts
@@ -2,11 +2,10 @@ import type { ConversationFlavor } from "@grammyjs/conversations"
 import type { HydrateFlavor } from "@grammyjs/hydrate"
 import type { ParseModeFlavor } from "@grammyjs/parse-mode"
 import type { ChatTypeContext, CommandContext, Context as TContext } from "grammy"
-import type { Chat } from "grammy/types"
+import type { Chat, Message } from "grammy/types"
 
 // NOTE: ParseModeFlavor must stay as the outer one
 export type Context = ParseModeFlavor<HydrateFlavor<ConversationFlavor<TContext>>>
-export type ConversationContext<CT extends Chat["type"]> = ChatTypeContext<
-  CommandContext<ParseModeFlavor<HydrateFlavor<TContext>>>,
-  CT
+export type ConversationContext<CT extends Chat["type"]> = ParseModeFlavor<
+  HydrateFlavor<CommandContext<ChatTypeContext<TContext, CT>>>
 >
