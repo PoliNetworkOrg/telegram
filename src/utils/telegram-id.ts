a7eedead3c6c5d3c3989bf7f242ca534f876c375
Lorenzo Corallo <66379281+lorenzocorallo@users.noreply.github.com>
2025-09-16 07:44:52 +0200

    fix: telegram username are not case-sensitive (#37)

--- src/utils/telegram-id.ts
+++ src/utils/telegram-id.ts
@@ -9,11 +9,11 @@ const usernameRedis = new RedisFallbackAdapter<number>({
 })
 
 export async function getTelegramId(username: string): Promise<number | null> {
-  const key = `${username.replaceAll("@", "")}:id`
+  const key = `${username.toLowerCase().replaceAll("@", "")}:id`
   return (await usernameRedis.read(key)) ?? null
 }
 
 export async function setTelegramId(username: string, id: number) {
-  const key = `${username}:id`
+  const key = `${username.toLowerCase()}:id`
   await usernameRedis.write(key, id)
 }
