854350aee1d96275e479ba102bf31fc0d66af673
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2025-09-24 20:02:02 +0200

    fix: username arg parsing

--- src/commands/role.ts
+++ src/commands/role.ts
@@ -1,22 +1,33 @@
+import { z } from "zod"
 import { api } from "@/backend"
 import { fmt } from "@/utils/format"
 import { getTelegramId } from "@/utils/telegram-id"
 import type { Role } from "@/utils/types"
-import { z } from "zod"
 
 import { _commandsBase } from "./_base"
+import { logger } from "@/logger"
 
 _commandsBase
   .createCommand({
     trigger: "getrole",
     scope: "private",
     description: "Get role of userid",
-    args: [{ key: "userId" }],
+    args: [
+      {
+        key: "username",
+        type: z.string().transform((s) => {
+          const n = Number(s)
+          if (!Number.isNaN(n) && s.trim() !== "") return n
+          return s
+        }),
+        description: "The username or the user id of the user you want to update the role",
+      },
+    ],
     handler: async ({ context, args }) => {
-      let userId: number | null = parseInt(args.userId, 10)
-      if (Number.isNaN(userId)) {
-        userId = await getTelegramId(args.userId.replaceAll("@", ""))
-      }
+      logger.debug({ a: args.username, type: typeof args.username })
+      const userId: number | null =
+        typeof args.username === "string" ? await getTelegramId(args.username.replaceAll("@", "")) : args.username
+
       if (userId === null) {
         await context.reply("Not a valid userId or username not in our cache")
         return
@@ -35,19 +46,23 @@ _commandsBase
     scope: "private",
     description: "Set role of username",
     args: [
-      { key: "username", description: "The username or the user id of the user you want to update the role" },
+      {
+        key: "username",
+        type: z.string().transform((s) => {
+          const n = Number(s)
+          if (!Number.isNaN(n) && s.trim() !== "") return n
+          return s
+        }),
+        description: "The username or the user id of the user you want to update the role",
+      },
       { key: "role", type: z.enum<Role[]>(["direttivo", "hr", "admin"]) },
     ],
     permissions: {
       allowedRoles: ["owner", "direttivo"],
     },
     handler: async ({ context, args }) => {
-      let userId: number | null = null
-      if (!Number.isNaN(args.username)) {
-        userId = await getTelegramId(args.username.replaceAll("@", ""))
-      } else {
-        userId = parseInt(args.username, 10)
-      }
+      const userId: number | null =
+        typeof args.username === "string" ? await getTelegramId(args.username.replaceAll("@", "")) : args.username
 
       if (userId === null) {
         await context.reply("Not a valid userId or username not in our cache")
