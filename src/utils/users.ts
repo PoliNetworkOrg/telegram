f10da09899ef24dae795615592dfe1ac7cc86aa4
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2026-01-30 00:12:35 +0100

    feat: support retrieving user from memoryStorage
    In the rare case when we need to get an user by userId,
    the context cannot retrieve it and the user is yet to be synced
    to the backend, we can retrieve it from the temporary map.

--- src/utils/users.ts
+++ src/utils/users.ts
@@ -1,25 +1,12 @@
 import type { Context } from "grammy"
 import type { User } from "grammy/types"
-import { err, ok, type Result } from "neverthrow"
-import { api } from "@/backend"
-import { logger } from "@/logger"
-import { toGrammyUser } from "./types"
+import { MessageUserStorage } from "@/middlewares/message-user-storage"
 
-export async function getUser<C extends Context>(userId: number, ctx?: C): Promise<Result<User, string>> {
+export async function getUser<C extends Context>(userId: number, ctx?: C): Promise<User | null> {
   const chatUser = await ctx
     ?.getChatMember(userId)
     .then((r) => r.user)
     .catch(() => null)
 
-  if (chatUser) return ok(chatUser)
-
-  try {
-    const { user, error } = await api.tg.users.get.query({ userId })
-    if (user) return ok(toGrammyUser(user))
-
-    return err(error)
-  } catch (error) {
-    logger.error({ error }, "getUser: error while calling the API")
-    return err("INTERNAL_SERVER_ERROR")
-  }
+  return chatUser ?? MessageUserStorage.getInstance().getStoredUser(userId)
 }
