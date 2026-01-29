94edd3e51075959aad36e0edb62efb1a0abaf559
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2026-01-30 00:03:08 +0100

    fix: handle API call error in getUser

--- src/utils/users.ts
+++ src/utils/users.ts
@@ -2,6 +2,7 @@ import type { Context } from "grammy"
 import type { User } from "grammy/types"
 import { err, ok, type Result } from "neverthrow"
 import { api } from "@/backend"
+import { logger } from "@/logger"
 import { toGrammyUser } from "./types"
 
 export async function getUser<C extends Context>(userId: number, ctx?: C): Promise<Result<User, string>> {
@@ -12,8 +13,13 @@ export async function getUser<C extends Context>(userId: number, ctx?: C): Promi
 
   if (chatUser) return ok(chatUser)
 
-  const { user, error } = await api.tg.users.get.query({ userId })
-  if (user) return ok(toGrammyUser(user))
+  try {
+    const { user, error } = await api.tg.users.get.query({ userId })
+    if (user) return ok(toGrammyUser(user))
 
-  return err(error)
+    return err(error)
+  } catch (error) {
+    logger.error({ error }, "getUser: error while calling the API")
+    return err("INTERNAL_SERVER_ERROR")
+  }
 }
