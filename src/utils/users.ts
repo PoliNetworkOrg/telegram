14928d2de2582c203e6b85bb9f21e664c7e2e078
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2026-01-30 02:02:36 +0100

    fix: getUser ctx undefined propagation in optional chaining

--- src/utils/users.ts
+++ src/utils/users.ts
@@ -2,11 +2,8 @@ import type { Context } from "grammy"
 import type { User } from "grammy/types"
 import { MessageUserStorage } from "@/middlewares/message-user-storage"
 
-export async function getUser<C extends Context>(userId: number, ctx?: C): Promise<User | null> {
-  const chatUser = await ctx
-    ?.getChatMember(userId)
-    .then((r) => r.user)
-    .catch(() => null)
-
-  return chatUser ?? MessageUserStorage.getInstance().getStoredUser(userId)
+export async function getUser<C extends Context>(userId: number, ctx: C | null): Promise<User | null> {
+  // TODO: check if this works correctly
+  const chatUser = ctx ? await ctx.getChatMember(userId).catch(() => null) : null
+  return chatUser?.user ?? MessageUserStorage.getInstance().getStoredUser(userId)
 }
