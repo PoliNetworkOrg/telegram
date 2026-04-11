a351fb7849ad972333e2ffbfb959fdb341e35f3f
Lorenzo Corallo <66379281+lorenzocorallo@users.noreply.github.com>
2026-04-11 16:54:24 +0200

    feat: pin and unpin commands (#83)
    * feat: pin and unpin commands
    
    * fix: remove repliedTo.from check, typo

---
+++ src/commands/pin.ts
@@ -0,0 +1,59 @@
+import { CommandsCollection } from "@/lib/managed-commands"
+import { logger } from "@/logger"
+import { fmt } from "@/utils/format"
+import { ephemeral } from "@/utils/messages"
+import type { Role } from "@/utils/types"
+
+export const pin = new CommandsCollection<Role>()
+  .createCommand({
+    trigger: "pin",
+    description: "Pin a message in a group",
+    scope: "group",
+    reply: "required",
+    permissions: {
+      allowGroupAdmins: true,
+      allowedRoles: ["direttivo", "owner"],
+    },
+    handler: async ({ context, repliedTo }) => {
+      const member = await context.getChatMember(context.me.id)
+      if (member.status !== "administrator")
+        return await ephemeral(context.reply(fmt(({ n }) => n`❌ The bot is not an admin`)), 10_000)
+
+      if (!member.can_pin_messages)
+        return await ephemeral(
+          context.reply(fmt(({ n, code }) => n`❌ The bot is missing the ${code`Pin messages`} permission.`)),
+          10_000
+        )
+
+      const res = await context.pinChatMessage(repliedTo.message_id).catch(() => false)
+      if (!res) return await ephemeral(context.reply(fmt(({ n }) => n`❌ Cannot pin the message`)), 10_000)
+
+      await ephemeral(context.reply(fmt(({ n }) => n`✅ Message pinned`)), 10_000)
+    },
+  })
+  .createCommand({
+    trigger: "unpin",
+    description: "Unpin a message in a group",
+    scope: "group",
+    reply: "required",
+    permissions: {
+      allowGroupAdmins: true,
+      allowedRoles: ["direttivo", "owner"],
+    },
+    handler: async ({ context, repliedTo }) => {
+      const member = await context.getChatMember(context.me.id)
+      if (member.status !== "administrator")
+        return await ephemeral(context.reply(fmt(({ n }) => n`❌ The bot is not an admin`)), 10_000)
+
+      if (!member.can_pin_messages)
+        return await ephemeral(
+          context.reply(fmt(({ n, code }) => n`❌ The bot is missing the ${code`Pin messages`} permission.`)),
+          10_000
+        )
+
+      const res = await context.unpinChatMessage(repliedTo.message_id).catch(() => false)
+      if (!res) return await ephemeral(context.reply(fmt(({ n }) => n`❌ Cannot unpin the message`)), 10_000)
+
+      await ephemeral(context.reply(fmt(({ n }) => n`✅ Message unpinned`)), 10_000)
+    },
+  })
