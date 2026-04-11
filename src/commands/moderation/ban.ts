aa05741640847cc7a27f0dda242b4782c7aa8b70
Lorenzo Corallo <66379281+lorenzocorallo@users.noreply.github.com>
2026-04-11 19:07:23 +0200

    feat: allow ban/mute first arg to be a user instead of repliedTo (#88)
    * feat: allow ban/mute first arg to be a user instead of repliedTo
    
    * fix: add a couple of catch
    
    * fix: typo

--- src/commands/moderation/ban.ts
+++ src/commands/moderation/ban.ts
@@ -6,27 +6,53 @@ import { fmt } from "@/utils/format"
 import { ephemeral } from "@/utils/messages"
 import { getTelegramId } from "@/utils/telegram-id"
 import { numberOrString, type Role } from "@/utils/types"
-import { getUser } from "@/utils/users"
+import { getOverloadUser, getUser } from "@/utils/users"
 
 export const ban = new CommandsCollection<Role>("Banning")
   .createCommand({
     trigger: "ban",
-    args: [{ key: "reason", optional: true, description: "Optional reason to ban the user" }],
+    args: [
+      {
+        key: "reasonOrUser",
+        optional: true,
+        description:
+          "If the message is a reply, this argument is the reason. Otherwise, it's the username or user id of the user to ban",
+        type: numberOrString,
+      },
+      { key: "reason", optional: true, description: "Optional reason to ban the user" },
+    ],
     description: "Permanently ban a user from a group",
     scope: "group",
-    reply: "required",
+    reply: "optional",
     permissions: {
       allowedRoles: ["owner", "direttivo"],
       excludedRoles: ["creator"],
       allowGroupAdmins: true,
     },
     handler: async ({ args, context, repliedTo }) => {
-      if (!repliedTo.from) {
-        logger.error("ban: no repliedTo.from field (the msg was sent in a channel)")
+      const userOverload = await getOverloadUser(context, repliedTo, args.reasonOrUser, args.reason)
+      if (userOverload.isErr()) {
+        await ephemeral(
+          context.reply(
+            repliedTo
+              ? fmt(({ n }) => n`There was an error`)
+              : fmt(({ n }) => n`Target user not found, please try replying to their message`)
+          )
+        )
+        logger.error({ args, repliedTo }, `BAN: ${userOverload.error}`)
         return
       }
 
-      const res = await Moderation.ban(repliedTo.from, context.chat, context.from, null, [repliedTo], args.reason)
+      const { user, reason } = userOverload.value
+
+      const res = await Moderation.ban(
+        user,
+        context.chat,
+        context.from,
+        null,
+        repliedTo ? [repliedTo] : undefined,
+        reason
+      )
       if (res.isErr()) await ephemeral(context.reply(res.error.fmtError))
     },
   })
